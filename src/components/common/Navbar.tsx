624c1dc667111648d87300e53d8ea7c6a3448536
Gabby <gabbzf@gmail.com>
2025-12-01 20:48:06 +0100

    Fix: code cleanup

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Home, PlusCircle, BookOpen, LogOut, Menu, X } from "lucide-react";
 
