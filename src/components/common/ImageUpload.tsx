9616fbd3b590bfff48b89317156eadb5d85b2a48
Gabby <gabbzf@gmail.com>
2025-12-03 17:36:45 +0100

    removed an unused import

--- src/components/common/ImageUpload.tsx
+++ src/components/common/ImageUpload.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { Upload, X, Image as ImageIcon } from "lucide-react";
+import { Upload, X } from "lucide-react";
 
 interface ImageUploadProps {
   currentImage?: string | null;
